d404fd516732f59e5a75f91af2b5150d6a80fc0e
Kenny Egun <kennyegun240@gmail.com>
2023-03-26 02:54:33 +0100

    Full app authentication wit WT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_request
+
+    SECRET_KEY = Rails.application.secret_key_base. to_s
+
+  def encode_token(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode_token(token)
+    decode = JWT.decode(token, SECRET_KEY).first
+    HashWithIndifferentAccess.new decode
+  end
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    token = header.split.last if header.present?
+    begin
+      decoded_token = decode_token(token)
+      @current_user_id = decoded_token['user_id']
+    rescue JWT::DecodeError
+      render json: { status: 'ERROR', message: 'Invalid token' }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(@current_user_id)
+  end
 end
