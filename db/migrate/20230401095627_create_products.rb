e8b3bbe909004aa3616d62d353028d5b5d0fb344
Kenny Egun <kennyegun240@gmail.com>
2023-04-03 17:58:41 +0100

    Product table migration generaated

---
+++ db/migrate/20230401095627_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :size
+      t.string :color
+      t.decimal :price
+      t.string :image
+      t.string :details
+      t.string :description
+      t.references :store, index: true, foreign_key: true
+      t.references :category, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
