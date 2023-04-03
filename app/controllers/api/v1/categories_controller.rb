4c4dc895df3c5eb89e0cded5a01dcbe6cb2a577e
Kenny Egun <kennyegun240@gmail.com>
2023-04-03 17:50:25 +0100

    Create category controller and modified it to query database and render information in json format

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::CategoriesController < ApplicationController
+  skip_before_action :authenticate_request, only: %i[index show_all show_cat_products]
+
+  def index
+    @category = Category.all.limit(3).includes(product: [:category, :store]).to_json(include: {product: {include: [:category, :store]}})
+    render json: {status: 'Success', message: '3 categories', data: @category}
+  end
+
+  def show_all
+    @category = Category.all.includes(product: [:category, :store]).to_json(include: {product: {include: [:category, :store]}})
+    render json: {status: 'Success', message: 'all categories', data: @category}
+  end
+
+  def show_cat_products
+    @category_products = Product.where(category_id: params[:id]).includes(:category)
+    render json: {status: 'Success', message: 'Gotten products from a particular category', data: @category_products}
+  end
+end
