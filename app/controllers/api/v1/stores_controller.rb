cd60737f71a2ef48749b7dd30d2bdd2823abcf98
Kenny Egun <kennyegun240@gmail.com>
2023-04-03 21:34:04 +0100

    Include login and signup methods

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::StoresController < ApplicationController
-  skip_before_action :authenticate_request, only: %i[index show_all show_store_products]
+  skip_before_action :authenticate_request, only: %i[index show_all show_store_products create login]
     def index
         @store = Store.all.limit(3)
         render json: {status: 'Success', message: '3 Stores gotten', data: @store}
@@ -13,5 +13,31 @@ class Api::V1::StoresController < ApplicationController
     def show_store_products
       @show_store_products = Product.where(store_id: params[:id]).includes(:store)
       render json: {status: 'Success', message: 'Gotten Items', data: @show_store_products}
+    end
+
+    def create
+      @store = Store.new(store_login_params)
+
+      if @store.save
+        token = encode_token(store_id: @store.id)
+        render json: {status: 'success', message: 'Store created', data: {token:}, store: @store}, status: 201
+      else
+        render json: {status: 'Error', message: 'Email exists already'}, status: 422
+      end
+    end
+
+    def login
+      @store = Store.find_by(email: params[:email])
+  
+      if @store && @store.authenticate(params[:password])
+        token = encode_token(store_id: @store.id)
+        render json: {status: 'Success', message: 'Logged in successfully', data: {token:}},status: 200
+      else
+        render json: {status: 'Error', message: 'Invalid email or password'},status: 401
+      end
+    end
+
+    def store_login_params
+      params.permit(:store_name, :email, :password)
     end
 end
