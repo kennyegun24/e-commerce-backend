70dc563021128215a14505653eb6250692bdcfce
Kenny Egun <kennyegun240@gmail.com>
2023-04-07 17:00:39 +0100

    Add seeded data

--- app/models/store.rb
+++ app/models/store.rb
@@ -4,29 +4,7 @@ class Store < ApplicationRecord
     has_many :product, foreign_key: 'store_id', class_name: 'Product'
 
     validates :store_name, presence: true, length: {maximum: 15}
-    # validates :image, presence: true
+    validates :image, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 6}
 end
-
-    # def create
-    #   # params.except(:store)
-    #   @store = Store.new(store_login_params)
-
-    #     puts('i am here')
-    #     if params[:image].present?
-    #       image_data = params[:image].tempfile.read
-    #       cloudinary_result = Cloudinary::Uploader.upload(image_data, resource_type: :auto)
-    #       image_url = cloudinary_result['secure_url']
-    #       @store.image = image_url
-    #     else
-    #     render json: { error: 'No image' }, status: :bad_request
-    #     return
-    #   end
-      
-    #   if @store.save
-    #     token = encode_token(store_id: @store.id)
-    #     render json: { status: 'success', message: 'Store created', data: { token: token }, store: @store }, status: :created
-    #   else
-    #     render json: { status: 'Error', message: 'Email exists already' }, status: :unprocessable_entity
-    #   end
