c8845b3360eec3c92f3fa9f30ec65415ebd05122
Kenny Egun <kennyegun240@gmail.com>
2023-04-13 20:24:23 +0100

    Add not null constraints to foreign keys

---
+++ db/migrate/20230412203031_add_not_null_to_columns.rb
@@ -0,0 +1,6 @@
+class AddNotNullToColumns < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :products, :store_id, false
+    change_column_null :products, :category_id, false
+  end
+end
