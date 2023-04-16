db418498aa244a26698a84b43484774e41e750da
Kenny Egun <kennyegun240@gmail.com>
2023-04-16 08:03:59 +0100

    Created order model, add validations and established relationships and other methods

---
+++ app/models/order.rb
@@ -0,0 +1,30 @@
+class Order < ApplicationRecord
+    belongs_to :store, class_name: 'Store'
+    belongs_to :user, class_name: 'User'
+    belongs_to :product, class_name: 'Product'
+
+    validate :check_store_id
+    validate :check_quantity
+
+    after_create :update_product
+
+    def update_product
+        if product.in_stock > self.quantity
+            product.update(in_stock: product.in_stock - self.quantity)
+        end
+    end
+
+    private
+  
+    def check_store_id
+      unless store_id == product.store_id
+        errors.add(:store_id, "must match the product's store_id")
+      end
+    end
+
+    def check_quantity
+        unless product.in_stock > self.quantity
+            errors.add(:quantity, 'Should not be negative')
+        end
+    end
+end
