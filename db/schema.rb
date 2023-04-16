e87e8fd3909b1928afd965ffe2b487e9bdc9a702
Kenny Egun <kennyegun240@gmail.com>
2023-04-16 08:05:06 +0100

    Order migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_204145) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_053104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_204145) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "product_name"
+    t.string "user_name"
+    t.string "image"
+    t.integer "quantity"
+    t.string "address"
+    t.text "tel_number"
+    t.bigint "user_id", null: false
+    t.bigint "store_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["store_id"], name: "index_orders_on_store_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -58,6 +75,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_204145) do
     t.boolean "admin"
   end
 
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "stores"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "stores"
 end
