5239eee3eb354bc7a9e14c49749114d13dedeb57
Kenny Egun <kennyegun240@gmail.com>
2023-04-19 11:21:28 +0100

    Add new method to update store all time value column

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,14 @@ class Product < ApplicationRecord
     belongs_to :category, class_name: 'Category'
 
     after_create :update_store
+    after_create :update_store_value
 
     def update_store
-        store.update(total_products: store.product.count)
+       store.update(total_products: self.store.product.count)
+    end
+
+    def update_store_value
+        new_store_value = store.store_value += (self.in_stock * self.price)
+        store.update(store_value: new_store_value)
     end
 end
