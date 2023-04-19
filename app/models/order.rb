c6ed822d309da596bd864bfd2efcb18d40555dbc
Kenny Egun <kennyegun240@gmail.com>
2023-04-19 11:20:23 +0100

    Add new method to update store total sold column

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,11 +7,16 @@ class Order < ApplicationRecord
     validate :check_quantity
 
     after_create :update_product
+    after_create :update_store_amount
 
     def update_product
-        if product.in_stock > self.quantity
+        if product.in_stock >= self.quantity
             product.update(in_stock: product.in_stock - self.quantity)
         end
+    end
+
+    def update_store_amount
+        store.update(total_sold: store.total_sold += self.amount)
     end
 
     private
@@ -23,7 +28,7 @@ class Order < ApplicationRecord
     end
 
     def check_quantity
-        unless product.in_stock > self.quantity
+        unless product.in_stock >= self.quantity
             errors.add(:quantity, 'Should not be negative')
         end
     end
