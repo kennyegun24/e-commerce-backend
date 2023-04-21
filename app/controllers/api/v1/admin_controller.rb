67c3853323e06e768e4cb0513260df6f28a28620
Kenny Egun <kennyegun240@gmail.com>
2023-04-21 18:55:10 +0100

    Add method to create new store, products and category

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AdminController < ApplicationController
-  skip_before_action :authenticate_request, only: %i[ login]
+  skip_before_action :authenticate_request, only: %i[ login create_category]
   def login
     @admin = Admin.find_by(email: params[:email])
 
@@ -30,5 +30,48 @@ class Api::V1::AdminController < ApplicationController
   def all_categories
     @categories = Category.all
     render json: {status: 'Success', message: 'All categories', categories: @categories}, status: 200
+  end
+
+  def create_category
+    @category = Category.new(categories_params)
+
+    if @category.save
+      render json: {status: 'success', message: 'Category created', category: @category}, status: 201
+    else
+      render json: @category, status: 422
+    end
+  end
+
+  def create_store
+    @store = Store.new(store_create_params)
+
+    if @store.save
+      token = encode_token(store_id: @store.id)
+      render json: {status: 'success', message: 'Store created', data: {token:}, store: @store}, status: 201
+    else
+      render json: {status: 'Error', message: 'Email exists already'}, status: 422
+    end
+  end
+
+  def create_product
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: {status: 'success', message: 'product created', product: @product}, status: 201
+    else
+      render json: {status: 'Error', message: 'Did not create'}, status: 422
+    end
+  end
+
+  def categories_params
+    params.require(:category).permit(:name, :image)
+  end
+  
+  def store_create_params
+    params.require(:store).permit(:store_name, :email, :password, :image)
+  end
+  
+  def product_params
+    params.require(:product).permit(:store_id, :in_stock, :category_id, :email, :name, :image, :size, :price, :color, :description, :details)
   end
 end
