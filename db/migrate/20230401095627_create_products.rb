f87e3b9f907b2ab796bf5d2244eb14a375420507
Kenny Egun <kennyegun240@gmail.com>
2023-04-30 20:44:39 +0100

    Modify migrations

--- db/migrate/20230401095627_create_products.rb
+++ db/migrate/20230401095627_create_products.rb
@@ -8,6 +8,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
       t.string :image
       t.string :details
       t.string :description
+      t.integer :in_stock
       t.references :store, index: true, foreign_key: true
       t.references :category, index: true, foreign_key: true
 
